dcc9a7622d1269b9baa9ab7ca90effa894108e77
Russell Toris <rctoris@wpi.edu>
2015-03-03 10:53:11 -0500

    comments for position and orientation

--- graspdb/include/graspdb/Position.h
+++ graspdb/include/graspdb/Position.h
@@ -1,3 +1,13 @@
+/*!
+ * \file Position.h
+ * \brief 3-point position information.
+ *
+ * A position simply contains x, y, and z value.
+ *
+ * \author Russell Toris, WPI - rctoris@wpi.edu
+ * \date March 3, 2015
+ */
+
 #ifndef RAIL_GRASPDB_POSITION_H_
 #define RAIL_GRASPDB_POSITION_H_
 
@@ -12,29 +22,91 @@ namespace pick_and_place
 namespace graspdb
 {
 
+/*!
+ * \class Position
+ * \brief 3-point position information.
+ *
+ * A position simply contains an x, y, and z value.
+ */
 class Position
 {
 public:
+  /*!
+   * \brief Create a new Position.
+   *
+   * Creates a new Position with the given x, y, and z values (defaults are 0).
+   * \param x The x value (default of 0).
+   * \param y The y value (default of 0).
+   * \param z The z value (default of 0).
+   */
   Position(const double x = 0, const double y = 0, const double z = 0);
 
+  /*!
+   * \brief Create a new Position.
+   *
+   * Creates a new Position with the given x, y, and z values from the ROS Point message.
+   * \param point The ROS Point message to extract values from.
+   */
   Position(const geometry_msgs::Point &point);
 
+  /*!
+   * \brief Create a new Position.
+   *
+   * Creates a new Position with the given x, y, and z values from the ROS Vector3 message.
+   * \param point The ROS Vector3 message to extract values from.
+   */
   Position(const geometry_msgs::Vector3 &v);
 
+  /*!
+   * \brief X value mutator.
+   *
+   * Set the x value of this Position.
+   * \param x The new x value.
+   */
   void setX(const double x);
 
+  /*!
+   * \brief X value accessor.
+   *
+   * Get the x value of this Position.
+   * \return The x value.
+   */
   double getX() const;
 
+  /*!
+   * \brief Y value mutator.
+   *
+   * Set the y value of this Position.
+   * \param y The new y value.
+   */
   void setY(const double y);
 
+  /*!
+   * \brief Y value accessor.
+   *
+   * Get the y value of this Position.
+   * \return The y value.
+   */
   double getY() const;
 
+  /*!
+   * \brief Z value mutator.
+   *
+   * Set the z value of this Position.
+   * \param z The new z value.
+   */
   void setZ(const double z);
 
+  /*!
+   * \brief Z value accessor.
+   *
+   * Get the z value of this Position.
+   * \return The z value.
+   */
   double getZ() const;
 
 private:
-  double x_, y_, z_;
+  double x_, y_, z_; /* Members to hold values. */
 };
 
 }
--- graspdb/include/graspdb/graspdb.h
+++ graspdb/include/graspdb/graspdb.h
@@ -1,3 +1,13 @@
+/*!
+ * \file graspdb.h
+ * \brief All-inclusive inclide file for the graspdb library.
+ *
+ * The graspdb library is a standalone C++ library for communicating with the PostgreSQL grasp database.
+ *
+ * \author Russell Toris, WPI - rctoris@wpi.edu
+ * \date March 3, 2015
+ */
+
 #ifndef RAIL_GRASPDB_GRASPDB_H_
 #define RAIL_GRASPDB_GRASPDB_H_
 
